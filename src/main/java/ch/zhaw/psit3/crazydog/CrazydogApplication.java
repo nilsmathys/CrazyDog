165ffbee25af9f3bdb5384b260dadf4faf96eb19
mathynil <mathynil@students.zhaw.ch>
2020-03-15 17:24:47 +0100

    Spring hinzugefügt

---
+++ src/main/java/ch/zhaw/psit3/crazydog/CrazydogApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.psit3.crazydog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazydogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazydogApplication.class, args);
+	}
+
+}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello crazy Dog!</h2>
-</body>
-</html>
