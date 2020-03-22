caf46e2ece212d75aceed973eb2c3572c257b8ec
mathynil <mathynil@students.zhaw.ch>
2020-03-22 10:42:15 +0100

    Creating class Player.java, Team.java & Piece.java

--- src/main/java/ch/zhaw/psit3/crazydog/Model/Piece.java
+++ src/main/java/ch/zhaw/psit3/crazydog/Model/Piece.java
@@ -1,4 +1,33 @@
-package ch.zhaw.psit3.crazydog.Model.Player;
+package ch.zhaw.psit3.crazydog.Model;
 
 public class Piece {
+    private int id;
+    private int number;
+    private String colour;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+
 }
--- src/main/java/ch/zhaw/psit3/crazydog/Model/Player/Player.java
+++ src/main/java/ch/zhaw/psit3/crazydog/Model/Player/Player.java
@@ -1,4 +1,21 @@
 package ch.zhaw.psit3.crazydog.Model.Player;
 
 public class Player {
+    private int id;
+    private String name;
+    private String email;
+    private String pw;
+    private int gamesWon;
+    private int gamesLost;
+    private int gamesPlayed;
+
+    public Player(String name, String email, String pw) {
+        this.name = name;
+        this.email = email;
+        this.pw = pw;
+    }
+
+    public void savePlayer() {
+
+    }
 }
