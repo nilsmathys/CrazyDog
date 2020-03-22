ceaabd42163d0589bbc3788faaf151a63d892e98
mathynil <mathynil@students.zhaw.ch>
2020-03-22 13:43:01 +0100

    Piece bearbeiten

--- src/main/java/ch/zhaw/psit3/crazydog/Model/Piece/Piece.java
+++ src/main/java/ch/zhaw/psit3/crazydog/Model/Piece/Piece.java
@@ -1,8 +1,15 @@
 package ch.zhaw.psit3.crazydog.Model.Piece;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Piece {
+    @NotNull
     private int id;
+
+    @Size(min = 1, max = 4)
     private int number;
+
     private String colour;
 
     public int getId() {
