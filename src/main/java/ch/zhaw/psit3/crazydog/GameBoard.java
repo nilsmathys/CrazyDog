1645ad2f9d3788f306285c9d4bbb7b2304ba4cbe
Riccardo Somma <sommaric@students.zhaw.ch>
2020-04-02 15:31:54 +0200

    GameBoard funktionen hinzugefügt

--- src/main/java/ch/zhaw/psit3/crazydog/GameBoard.java
+++ src/main/java/ch/zhaw/psit3/crazydog/GameBoard.java
@@ -27,7 +27,23 @@ public class GameBoard {
     }
 
     public void changePictureOnField(int index, String pictureName) {
+        if(index < 0 || index > fields.length)
+        {
+            throw new IllegalArgumentException("Please specify a correct index");
+        }
         fields[index].setImageName(pictureName);
     }
 
+    public GameField[] getFields()
+    {
+        return fields;
+    }
+
+    public GameField getSpecificField(int index) {
+        if(index < 0 || index > fields.length)
+        {
+            throw new IllegalArgumentException("Please specify a correct index");
+        }
+        return fields[index];
+    }
 }
