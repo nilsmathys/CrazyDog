f284600fbc4d884a760fb247e78d02f20947fdf2
Remo Bertschinger <bertsrem@students.zhaw.ch>
2020-04-19 11:02:50 +0200

    Finish the login logic without the session logic.

--- src/main/java/ch/zhaw/psit3/crazydog/Controller/LoginController.java
+++ src/main/java/ch/zhaw/psit3/crazydog/Controller/LoginController.java
@@ -1,6 +1,7 @@
 package ch.zhaw.psit3.crazydog.Controller;
 
 import ch.zhaw.psit3.crazydog.Model.Player.Player;
+import ch.zhaw.psit3.crazydog.Model.Player.PlayerDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,22 +16,20 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginSubmit(@ModelAttribute Player player) {
-        System.out.println(player.getUsername());
-        System.out.println(player.getPassword());
-        return "login";
-    }
-/*
-    @RequestMapping(value = "/checklogindata", method = RequestMethod.POST)
-    public RedirectView checkLoginData(RedirectAttributes attributes) {
-        System.out.println("/checklogindata wurde angesprochen");
-        attributes.addFlashAttribute("error", "Email oder Passwort ist falsch");
+    public String loginSubmit(@ModelAttribute Player player, Model model) {
+        // Try to get the User Data out of the data base
+        player = PlayerDAO.getPlayerByUsernameAndPw(player.getUsername(), player.getPassword());
 
-        Player player = PlayerDAO.getPlayerByUsernameAndPw("Spieler1", "test123");
-        System.out.println(player.getUsername());
-        System.out.println(player.getPw());
-
-        return new RedirectView("login");
+        // Check if the user exists. If the player ID is 0, the user doesn't exist in the database.
+        if(player.getId() != 0) {
+            // Set session
+            return "index";
+        }
+        else {
+            System.out.println("This user doesn't exist.");
+            model.addAttribute("loginerror", "Email or Password was wrong");
+            model.addAttribute("player", new Player()); // Return empty player to reset the HTML form
+            return "login";
+        }
     }
-*/
 }
