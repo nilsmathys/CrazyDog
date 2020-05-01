d60f983b953a748536966d821640bf55c40a6c1b
Remo Bertschinger <bertsrem@students.zhaw.ch>
2020-05-01 22:32:10 +0200

    Add Message Class. This Class is used to create Message Objects, which will be parsed to JSON Objects and returned to the javascript function sendCardAndIdAndDestination()

---
+++ src/main/java/ch/zhaw/psit3/crazydog/Model/Message/Message.java
@@ -0,0 +1,14 @@
+package ch.zhaw.psit3.crazydog.Model.Message;
+
+public class Message {
+
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
