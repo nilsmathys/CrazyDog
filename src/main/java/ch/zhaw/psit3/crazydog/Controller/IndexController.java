5cf7970bd2d78f060fa7995a57d8cc4030a94b57
Riccardo Somma <sommaric@students.zhaw.ch>
2020-05-01 20:36:05 +0200

    format login.html and index.html and edit their controllers

--- src/main/java/ch/zhaw/psit3/crazydog/Controller/IndexController.java
+++ src/main/java/ch/zhaw/psit3/crazydog/Controller/IndexController.java
@@ -1,13 +1,37 @@
 package ch.zhaw.psit3.crazydog.Controller;
 
+import ch.zhaw.psit3.crazydog.Model.Player.Player;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(HttpServletRequest request, Model model) {
+
+        if(request.getSession().getAttribute("id") != null) {
+
+        } else {
+            model.addAttribute("player", new Player());
+            model.addAttribute("loginerror", "Please login to play a game");
+            return "login";
+        }
+        return "index";
+    }
+    @GetMapping("/")
+    public String wurzel(HttpServletRequest request, Model model) {
+
+        if(request.getSession().getAttribute("id") != null) {
+
+        } else {
+            model.addAttribute("player", new Player());
+            model.addAttribute("loginerror", "Please login to play a game");
+            return "login";
+        }
         return "index";
     }
 }
