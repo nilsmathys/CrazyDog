a641a579348d7cfc7804394882313b2a2cc45f47
Riccardo Somma <sommaric@students.zhaw.ch>
2020-05-01 21:38:00 +0200

    add user manual on website

---
+++ src/main/java/ch/zhaw/psit3/crazydog/Controller/ManualController.java
@@ -0,0 +1,31 @@
+package ch.zhaw.psit3.crazydog.Controller;
+
+import ch.zhaw.psit3.crazydog.Model.Player.Player;
+import ch.zhaw.psit3.crazydog.Model.Player.PlayerDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ManualController {
+
+    @GetMapping("/spielanleitung")
+    public String index(HttpServletRequest request, Model model) {
+
+        if(request.getSession().getAttribute("id") != null) {
+
+        } else {
+            model.addAttribute("player", new Player());
+            model.addAttribute("loginerror", "Please login first");
+            return "login";
+        }
+        return "spielanleitung";
+    }
+
+}
