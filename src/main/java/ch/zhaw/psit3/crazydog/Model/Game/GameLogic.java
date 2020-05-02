b23ff074a8f76706e02597ab51629bb882397a2e
Remo Bertschinger <bertsrem@students.zhaw.ch>
2020-05-02 12:44:52 +0200

    Add two new methods to GameLogic Class: - getGameFieldsFromGameBoard()   Is responsible for always pulling the newwst GameFieldList from GameBoard - getPlayersColorFromId(int id)   Is responsible for getting the color of a player, when we put his id into the function

--- src/main/java/ch/zhaw/psit3/crazydog/Model/Game/GameLogic.java
+++ src/main/java/ch/zhaw/psit3/crazydog/Model/Game/GameLogic.java
@@ -4,17 +4,24 @@ import ch.zhaw.psit3.crazydog.CrazyDog;
 import ch.zhaw.psit3.crazydog.Model.GameField.GameBoard;
 import ch.zhaw.psit3.crazydog.Model.GameField.GameField;
 import ch.zhaw.psit3.crazydog.Model.Message.Message;
+import ch.zhaw.psit3.crazydog.Model.Player.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class GameLogic {
-    static Message successmessage;
-    static ArrayList<GameField> gameFieldList = new ArrayList<>();
+    private static Message successmessage;
+    private static List<GameField> gameFieldList;       // This is a copy of List<GameField> from Gameboard
 
     // Is responsible for returning a list
     public static void calculateDestinations(int cardValue, int sessionId) {
+        getGameFieldsFromGameBoard();
+        System.out.println(getPlayersColorFromId(1));
+        System.out.println(getPlayersColorFromId(2));
+        System.out.println(getPlayersColorFromId(3));
+        System.out.println(getPlayersColorFromId(4));
+
         gameFieldList.clear();
         Random rand = new Random();
         GameField gameField1 = new GameField("field" + rand.nextInt(96));
@@ -27,13 +34,17 @@ public class GameLogic {
         gameFieldList.add(gameField4);
 
         // How to access the List with fields, which will be needed to calculate the destinations
-        List<GameField> fields = CrazyDog.getGameBoard().getFields();
-        for(GameField field : fields) {
-            //System.out.println(field.getImageName());
-        }
+        //List<GameField> fields = CrazyDog.getGameBoard().getFields();
+        //for(GameField field : fields) {
+        //    System.out.println(field.getGameFieldName());
+        //    System.out.println(field.getImageName());
+        //    System.out.println(field.getCssId());
+        //    System.out.println(field.getColor());
+        //    System.out.println("----------------------------");
+        //}
     }
 
-    public static ArrayList<GameField> getDestinations() {
+    public static List<GameField> getDestinations() {
         return gameFieldList;
     }
 
@@ -43,5 +54,24 @@ public class GameLogic {
 
     public static Message getSuccessMessage() {
         return successmessage;
+    }
+
+    // INTERNAL CLASSES ONLY USED BY GAMELOGIC (private)
+
+    // Responsible for getting the most recent version of the GameFieldList from GameBoard
+    private static void getGameFieldsFromGameBoard() {
+        gameFieldList = CrazyDog.getGameBoard().getFields();
+    }
+
+    // Responsible for getting a Players Color, when we know his id
+    private static String getPlayersColorFromId(int id) {
+        List<Player> playerList = CrazyDog.getPlayerList();
+        String color = "";
+        for(Player player : playerList) {
+            if(player.getId() == id) {
+                color = player.getColor();
+            }
+        }
+        return color;
     }
 }
