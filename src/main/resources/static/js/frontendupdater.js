cbbff7c6ee49e9123fac4dc41ac3bad6735bc4e9
Stefanie Werlin <werliste@students.zhaw.ch>
2020-05-04 00:34:46 +0200

    game-Seite responsive umgesetzt; normale Browseransicht neu bei 100%

--- src/main/resources/static/js/frontendupdater.js
+++ src/main/resources/static/js/frontendupdater.js
@@ -99,7 +99,32 @@ $(function updateButtonBlock() {
             $("#buttonBlock").replaceWith(fragment);
         },
         complete: function() {
-            setTimeout(updateButtonBlock, 5000);
+            setTimeout(updateButtonBlock, 40000);
         }
     });
+});
+
+jQuery(document).ready(function($){
+    zoom();
+
+    $( window ).resize(function() {
+        zoom();
+    });
+
+    function zoom() {
+        let htmlWidth = $('html').innerWidth();
+        let bodyWidth = 900;
+
+        let scale;
+        let height;
+        if (htmlWidth > bodyWidth) {
+            scale = 0.5;
+            height = 450;
+        } else {
+            scale = htmlWidth / bodyWidth / 2;
+            height = 900 * scale;
+        }
+        $('.container-top').css('transform', 'scale(' + scale + ')');
+        $('.playarea').css('height', height +'px' )
+    }
 });
